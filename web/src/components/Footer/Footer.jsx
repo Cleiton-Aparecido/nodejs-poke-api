80c3f16d3a6311981be996dbe8e9e6c049e63771
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-01-05 21:27:22 -0300

    Styling footer

--- web/src/components/Footer/Footer.jsx
+++ web/src/components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ export function Footer () {
             <div className="footer__text-box">
 
                 <div className="footer__text">
-                    TEXT 1
+                Nunc nibh arcu, cursus at erat et, venenatis faucibus risus. Sed non finibus odio, et varius turpis. Quisque vel sollicitudin felis. Cras aliquet libero ipsum, non accumsan leo convallis nec. Nulla sem magna, imperdiet id lectus sed, luctus sodales est. Nulla nec lectus sollicitudin, fringilla orci vel, blandit turpis. Pellentesque eu arcu non quam aliquet tristique. Vivamus eu leo nisi. Suspendisse eget cursus nunc. In a mauris aliquet, finibus nisi eu, bibendum magna
                 </div>
 
                 <div className="footer__text">
-                    TEXT 2
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut molestie dui nec tortor bibendum, vitae tempus velit mollis. Nunc sagittis dolor sit amet sapien sollicitudin varius. Proin commodo nisi vitae lorem ornare, porttitor luctus ex ullamcorper. Donec iaculis quis ligula eget pulvinar. Etiam id bibendum purus, ac dapibus ipsum. In tristique ullamcorper leo, sed congue ligula suscipit sit amet. Morbi a facilisis lacus. Morbi commodo, nibh vel sagittis pellentesque, lorem lectus dapibus felis,
                 </div>
 
             </div>
--- web/src/components/SearchContainer/SearchContainer.jsx
+++ web/src/components/SearchContainer/SearchContainer.jsx
@@ -1,9 +1,11 @@
+const placeholder = "Search a Pokémon here..."
+
 export function SearchContainer () {
     return (
         <div className="search-container">
             <img src="../../public/hero.png" className="search-container__hero"/>
 
-            <input className="search-container__input input" type={"text"} placeholder="Search Here"></input>
+            <input className="search-container__input input" type={"text"} placeholder={placeholder}></input>
 
             <button className="search-container__btn btn">Search</button>
         </div>
