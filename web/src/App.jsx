0b56d4c23da10b3bdc41bcc1cadd08aaf3eff5e6
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-01-06 12:05:04 -0300

    first API cosuming

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,11 +4,33 @@ import '../src/styles/css/style.css'
 
 import { THEME } from './styles/variables'
 
+import axios from 'axios'
+
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { Footer } from './components/Footer/Footer'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [pokemon, setPokemon] = useState("pikachu")
+  const [pokemonData, setPokemonData] = useState([])
+  const [pokemonType, setPokemonType] = useState("")
+
+  const getPokemon = async () => {
+    const toArray = []
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const res = await axios.get(url)
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getPokemon()
+  }, [])
 
   return (
     
